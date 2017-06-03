0138e574a60ec6030076bb9ca4d1029ce19b399d
pablojoju <pcorteslorenzo@gmail.com>
2017-06-03 13:35:37 +0200

    Added baseSkill.cs script

--- UnityProject/Assets/Modules/Skills/BaseSkill.cs
+++ UnityProject/Assets/Modules/Skills/BaseSkill.cs
@@ -2,15 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BaseSkill : MonoBehaviour {
+namespace Skill
+{
+    public abstract class BaseSkill
+    {
+        public SkillData Data;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+        public bool SelectingTarget;
+
+        private float m_cooldownControlTime;
+
+        public abstract void Execute();
+        public abstract void Cancel();
+    }
+
+    public class SkillData
+    {
+        public string ID;
+        public string Name;
+        public string Description;
+
+        public string ImagePath;
+        public bool AllowMultipleTargets;
+        public int TargetCountLimit;
+
+        public bool NeedTileTarget;
+        public bool NeedPawnTarget;
+        public bool NeedLootTarget;
+        public bool NeedGatherTarget;
+
+        public float CooldownTime;
+    }
 }
